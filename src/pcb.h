72cc43c17f8deabf31e8948ea67d1d9f68f20402
MichaelRyanJr <michaeljohnryanjr@gmail.com>
2026-03-06 17:20:28 -0500

    Update pcb.h
    Added initial function headers and struct variables

--- src/pcb.h
+++ src/pcb.h
@@ -1,1 +1,26 @@
+#ifndef PCB_H
+#define PCB_H
 
+typedef enum {
+  NEW,
+  READY,
+  RUNNING,
+  TERMINATED
+} ProcessState;
+
+typedef struct {
+  char pid[20];
+  int arrivalTime;
+  int burstTime;
+  int remainingTime;
+  int priority;
+  int startTime;
+  int completionTime;
+  ProcessState state;
+} PCB;
+
+void initPCB(PCB *p, char *pid, int arrival, int burst, int priority);
+char stateToString(ProcessState state);
+void printPCB(FILE *fp, PCB *p);
+
+#endif
