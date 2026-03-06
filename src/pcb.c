df6aeda786d8e38c21e4b148a52f23db2ecd7dca
MichaelRyanJr <michaeljohnryanjr@gmail.com>
2026-03-06 17:55:05 -0500

    Update pcb.c
    added stateToString and printPCB functions

--- src/pcb.c
+++ src/pcb.c
@@ -11,3 +11,24 @@ void initPCB(PCB *p, char *pid, int arrival, int burst, int priority){
   p->completionTime = -1;
   p->state = NEW;
 }
+
+const char *stateToString(ProcessState state){
+  if(state == NEW){
+    return "NEW";
+  }
+  else if(state == READY){
+    return "READY";
+  }
+  else if(state == RUNNING){
+    return "RUNNING";
+  }
+  else if(state == TERMINATED){
+    return "TERMINATED";
+  }
+
+  return "ERROR";
+}
+
+void printPCB(FILE *fp, PCB *p){
+  fprintf(fp, "PID=%s Arr=%d Burst=%d Rem=%d Prio=%d State=%s\n", p->pid, p->arrivalTime, p->burstTime, p->remainingTime, p->priority, stateToString(p->state));
+}
