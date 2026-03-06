15f63fa1d3e9f362a62b65d77b6c8bb4c7f01f75
MichaelRyanJr <michaeljohnryanjr@gmail.com>
2026-03-06 18:08:39 -0500

    Update queue.h
    Added struct variables and function headers

--- src/queue.h
+++ src/queue.h
@@ -1,1 +1,18 @@
+#ifndef QUEUE_H
+#define QUEUE_H
 
+typedef struct {
+  int *data;
+  int size;
+  int capacity;
+} Queue;
+
+void initQueue(Queue *q, int capacity);
+void freeQueue(Queue *q);
+int isEmpty(Queue *q);
+void giveQueue(Queue *q, int value);
+int takeQueue(Queue *q);
+int takeAt(Queue *q, int pos);
+int getAt(Queue *q, int pos);
+
+#endif
