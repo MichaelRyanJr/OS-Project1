7f900e561627eb80697faec095a7250387c44c42
MichaelRyanJr <michaeljohnryanjr@gmail.com>
2026-03-06 17:42:20 -0500

    Update pcb.c
    Added init function

--- src/pcb.c
+++ src/pcb.c
@@ -1,1 +1,13 @@
+#include "pcb.h"
+#include <string.h>
 
+void initPCB(PCB *p, char *pid, int arrival, int burst, int priority){
+  strcpy(p->pid, pid);
+  p->arrivalTime = arrival;
+  p->burstTime = burst;
+  p->remainingTime = burst;
+  p->priority = priority;
+  p->startTime = -1;
+  p->completionTime = -1;
+  p->state = NEW;
+}
