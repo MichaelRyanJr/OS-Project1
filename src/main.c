8070f9076dbe7b6bff258401cb60c596eb4d95b9
MichaelRyanJr <michaeljohnryanjr@gmail.com>
2026-03-07 17:52:40 -0500

    Update main.c
    Created a test program to read processes from input.txt and output them to output_test.txt

--- src/main.c
+++ src/main.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include "pcb.h"
 
+int main(void){
+  FILE *inFile;
+  FILE *outFile;
+
+  PCB processes[100];
+  int count = 0;
+
+  char header[100];
+  char pid[20];
+  int arrival;
+  int burst;
+  int priority;
+
+  inFile = fopen("input.txt", "r");
+
+  fgets(header, sizeof(header), inFile);
+  while(fscanf(inFile, "%s %d %d %d", pid, &arrival, &burst, &priority) == 4){
+    initPCB(&processes[count], pid, arrival, burst, priority);
+    count++;
+  }
+  fclose(inFile);
+
+  outFile = fopen("output_test.txt", "w");
+
+  fprintf(outFile, "Test of printPCB():\n");
+  for(int i = 0; i < count; i++){
+    printPCB(outFile, &processes[i]);
+  }
+  fclose(outFile);
+
+  return 0;
+}
