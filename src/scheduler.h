c1fe0898fca9f0bda07c88ccbe8d0ae2f2e47035
MichaelRyanJr <michaeljohnryanjr@gmail.com>
2026-03-07 18:13:15 -0500

    Update scheduler.h
    Added preliminary function headers

--- src/scheduler.h
+++ src/scheduler.h
@@ -1,1 +1,12 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
 
+#include <stdio.h>
+#include "pcb.h"
+#include "queue.h"
+
+void runFCFS(FILE *outFile, PCB processes[], int count);
+int allTerminated(PCB processes[], int count);
+void printTimeStep(FILE *outFile, int time, PCB processes[], int runningIndex, Queue *readyQueue);
+
+#endif
