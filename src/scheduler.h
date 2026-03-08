725d4ea9524bf3678a278870eb34089017d1760d
MichaelRyanJr <michaeljohnryanjr@gmail.com>
2026-03-08 16:47:39 -0400

    Update scheduler.h
    added runRR function header

--- src/scheduler.h
+++ src/scheduler.h
@@ -8,6 +8,7 @@
 void runFCFS(FILE *outFile, PCB processes[], int count);
 void runPriority(FILE *outFile, PCB processes[], int count);
 void runSRTF(FILE *outFile, PCB processes[], int count);
+void runRR(FILE *outFile, PCB processes[], int count);
 int allTerminated(PCB processes[], int count);
 void printTimeStep(FILE *outFile, int time, PCB processes[], int runningIndex, Queue *readyQueue);
 void printGanttChart(FILE *outFile, PCB processes[], int gantt[], int ganttLength);
