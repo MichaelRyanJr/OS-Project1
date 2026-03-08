e6c443090dae0884d4c86998f63c1d4b415fa53a
MichaelRyanJr <michaeljohnryanjr@gmail.com>
2026-03-08 15:25:34 -0400

    Update scheduler.h
    Added runSRTF function header

--- src/scheduler.h
+++ src/scheduler.h
@@ -7,6 +7,7 @@
 
 void runFCFS(FILE *outFile, PCB processes[], int count);
 void runPriority(FILE *outFile, PCB processes[], int count);
+void runSRTF(FILE *outFile, PCB processes[], int count);
 int allTerminated(PCB processes[], int count);
 void printTimeStep(FILE *outFile, int time, PCB processes[], int runningIndex, Queue *readyQueue);
 void printGanttChart(FILE *outFile, PCB processes[], int gantt[], int ganttLength);
